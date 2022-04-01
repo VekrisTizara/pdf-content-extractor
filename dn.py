6d61334d907307f6e177fe3f70abf0a5f6a05580
VekrisTizara <94655538+VekrisTizara@users.noreply.github.com>
2022-04-01 19:53:57 +0200

    Update dn.py
    delete unnecessary print

--- dn.py
+++ dn.py
@@ -4,7 +4,6 @@ def find_pdf():
     list_pdfs = []
     for file in glob.glob('**/*.pdf', recursive=True):
         list_pdfs.append(file)
-        print(file)
     return list_pdfs
 
 print(find_pdf())
